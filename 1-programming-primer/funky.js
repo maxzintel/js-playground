558d87465e6fa65e9e732516a0db5856df6708e4
s1dequest <maxzintel@gmail.com>
2020-04-05 16:38:19 -0400

    First pass on exercise 1 solution.

--- 1-programming-primer/funky.js
+++ 1-programming-primer/funky.js
@@ -1,1 +1,12 @@
-console.log("yo");
+var favoriteBooks = [];
+
+function addFavoriteBook(bookName) {
+  if (bookName.includes("Great")) {
+    favoriteBooks.push(bookName);
+  } else {
+    console.log("Book not added to array.")
+  };
+}
+
+addFavoriteBook("The Great Gatsby");
+console.log(favoriteBooks);
