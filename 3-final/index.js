37700551c6a4f240a04c53dddb7a69750052cc73
s1dequest <maxzintel@gmail.com>
2020-04-13 17:17:53 -0400

    Explanation on the callback used in the exercise.

--- 3-final/index.js
+++ 3-final/index.js
@@ -11,7 +11,7 @@ class Bookshelf {
   }
 
   printFavoriteBooks() {
-    console.log(`Favorite Books: ${this.favoriteBooks.length}`);
+    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
     for (let bookName of this.favoriteBooks) {
       console.log(String(bookName));
     }
@@ -19,7 +19,7 @@ class Bookshelf {
 }
 
 function loadBooks() {
-  fakeAjax(BOOK_API, (bookNames) => {
+  fakeAjax(BOOK_API, (bookNames) => { // invoked whenever cb is called, cb passes it the array of book names.
     for (let bookName of bookNames) {
       shelf.addFavoriteBook(bookName);
     }
@@ -31,9 +31,9 @@ var BOOK_API = "https://some.url/api";
 var shelf = new Bookshelf();
 loadBooks();
 
-function fakeAjax(url,cb) {
+function fakeAjax(url,cb) { // cb here = callback, which is a function we invoke.
 	setTimeout(function fakeLoadingDelay(){
-		cb([
+		cb([ // function is called with an array of book names.
 			"A Song of Ice and Fire",
 			"The Great Gatsby",
 			"Crime & Punishment",
@@ -42,3 +42,7 @@ function fakeAjax(url,cb) {
 		]);
 	},500);
 }
+
+// Okay, fakeAjax is a super old way of doing what we now using promises and async/await for.
+// Basically, whenever the cb parameter is called, it returns the results of the callback cb function in the fakeAjax definition.
+// Thus, bookNames in the loadBooks function is = to the cb array.
